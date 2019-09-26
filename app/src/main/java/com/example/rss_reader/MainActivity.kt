9edd114479fb10765a3678566ea84bdad089648d
Ruslan <wazsone@ya.ru>
2019-09-26 22:25:58 +0700

    git init

---
+++ app/src/main/java/com/example/rss_reader/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rss_reader
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

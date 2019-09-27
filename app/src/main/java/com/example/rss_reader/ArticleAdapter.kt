be065459aea5f94ecfa3562c754c9198b2d691f3
Ruslan <wazsone@ya.ru>
2019-09-27 23:59:07 +0700

    add the second news source
    also: change date format, add sorting news by date

--- app/src/main/java/com/example/rss_reader/ArticleAdapter.kt
+++ app/src/main/java/com/example/rss_reader/ArticleAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.rss_reader
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +8,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.prof.rssparser.Article
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.article_item.view.*
+import java.lang.Exception
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ArticleAdapter(private val articles: MutableList<Article>) :
     RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
@@ -25,8 +30,26 @@ class ArticleAdapter(private val articles: MutableList<Article>) :
         holder.bind(articles[position])
 
     inner class ArticleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val TAG = ArticleViewHolder::class.simpleName.toString()
         fun bind(article: Article) {
-            itemView.article_item__date.text = article.pubDate
+            itemView.article_item__date.text = try {
+                val sourceDateString = article.pubDate.toString()
+                //Tue, 10 Sep 2019 12:21:23 +0300
+                val inputPattern = SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH)
+                val date = try {
+                    inputPattern.parse(sourceDateString)
+                } catch (e: java.lang.Exception) {
+                    Log.d(TAG, "Couldn't parse pubData by the 'EEE, d MMM yyyy HH:mm:ss Z' format. StackTrace: ${e.stackTrace}")
+                    //Fri, 27 Sep 2019 14:32:25 GMT
+                    val inputPattern2 = SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.ENGLISH)
+                    inputPattern2.parse(sourceDateString)
+                }
+                val outputPattern = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+                outputPattern.format(date)
+            } catch (e: ParseException) {
+                Log.d(TAG, "Couldn't parse pubData. StackTrace: ${e.stackTrace}")
+                article.pubDate
+            }
             itemView.article_item__title.text = article.title
             Picasso.get().load(article.image)
                 .placeholder(R.drawable.placeholder)
--- app/src/main/java/com/example/rss_reader/MainActivity.kt
+++ app/src/main/java/com/example/rss_reader/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.rss_reader
 
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.UiThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -11,10 +13,15 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = MainActivity::class.simpleName.toString()
 
-    private val url = "https://meduza.io/rss/podcasts/meduza-v-kurse"
+    private val url1 = "https://meduza.io/rss/podcasts/meduza-v-kurse"
+    private val url2 = "https://habr.com/ru/rss/hubs/all/"
+
     private val job = Job()
     private val coroutinScope = CoroutineScope(Dispatchers.Main + job)
     private lateinit var adapter: ArticleAdapter
@@ -35,11 +42,26 @@ class MainActivity : AppCompatActivity() {
 
     private fun fetchFeed() {
         coroutinScope.launch {
-            val articles = try {
+            val articles = mutableListOf<Article>()
+            try {
                 val parser = Parser()
-                parser.getArticles(url)
+                articles.addAll(parser.getArticles(url1))
+                articles.addAll(parser.getArticles(url2))
+                articles.sortByDescending {
+                    val sourceDateString = it.pubDate.toString()
+                    //Tue, 10 Sep 2019 12:21:23 +0300
+                    val inputPattern = SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z", Locale.ENGLISH)
+                    try {
+                        inputPattern.parse(sourceDateString)
+                    } catch (e: java.lang.Exception) {
+                        Log.d(TAG, "Couldn't parse pubData by the 'EEE, d MMM yyyy HH:mm:ss Z' format. StackTrace: ${e.stackTrace}")
+                        //Fri, 27 Sep 2019 14:32:25 GMT
+                        val inputPattern2 = SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.ENGLISH)
+                        inputPattern2.parse(sourceDateString)
+                    }
+                }
             } catch (e: Exception) {
-                mutableListOf<Article>()
+                Log.d(TAG, "Couldn't load news. StackTrace: ${e.stackTrace}")
             }
             adapter = ArticleAdapter(articles)
             main__rv_articles.adapter = adapter
